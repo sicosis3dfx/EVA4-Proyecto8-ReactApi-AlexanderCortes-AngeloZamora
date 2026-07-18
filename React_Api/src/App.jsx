1a12dfbb98447084f373fd8d68aa784a68dac837
sicosis3dfx <sicosis3dfx@outlook.com>
2026-07-18 02:13:15 -0400

    Update App.jsx

--- React_Api/src/App.jsx
+++ React_Api/src/App.jsx
@@ -1,25 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import AgregarPedido from './pages/AgregarPedido' // Tu landing original vive aquí
-import VerPedidos from './pages/VerPedidos'       // La cocina del profesor
-import { EditarPedido } from './pages/EditarPedido' // El editor de estados de la API
+import AgregarPedido from './pages/AgregarPedido'
+import VerPedidos from './pages/VerPedidos'       
+import { EditarPedido } from './pages/EditarPedido'
 import './App.css'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      
-      <Navbar />
-      
+    <BrowserRouter>      
+      <Navbar />     
       <main>
-        <Routes>
-          
-          <Route path="/" element={<AgregarPedido />} />
-          
-          
-          <Route path="/ver" element={<VerPedidos />} />
-          
-          
+        <Routes>         
+          <Route path="/" element={<AgregarPedido />} />          
+          <Route path="/ver" element={<VerPedidos />} /> 
           <Route path="/editar/:id" element={<EditarPedido />} />
         </Routes>
       </main>
